b329ebbf375e99606b1f4d65d1ad0392e4f87f7b
jitbherwaniofficial <jitbherwani92@gmail.com>
2025-02-11 00:57:57 +0530

    MLS Updated3

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,13 @@
 
-echo " BUILD START"
+echo "BUILD START"
+
+# Create and activate a virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Upgrade pip and install dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
-echo " BUILD END"
+
+
+echo "BUILD END"
